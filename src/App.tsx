8f84baf7a6216c4f289a3e508d153c6ff0a062a8
Juan Camilo Duque <kcamilito@gmail.com>
2024-10-17 07:26:14 -0500

    Agrego react-hook-form para validar formulario y creo type para el formulario

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
+import {PatientForm,PatientsList} from './components';
 
 function App() {
 
   return (
     <>
-      <h1 className="text-center text-9xl font-sans text-indigo-900 font-bold">Pacientes</h1>
+      <div className="container mx-auto mt-20">
+        <h1 className="text-center text-4xl font-black md:w-2/3 md:mx-auto">Seguimiento de pacientes {''}
+          <span className="text-indigo-700" >Veterinaria</span>
+        </h1>
+      </div>
+      <div className="mt-12 md:flex">
+        <PatientForm/>
+        <PatientsList/>
+      </div>
     </>
   )
 }
